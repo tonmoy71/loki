3ed49be933ad7634e09cd00df9c10581a7efb53f
Fahim Masud Choudhury <tonmoy71@gmail.com>
2015-01-02 11:50:07 +0600

    MapView is added and working.

--- app/src/main/java/co/gobd/loki/loki/MainActivity.java
+++ app/src/main/java/co/gobd/loki/loki/MainActivity.java
@@ -2,30 +2,38 @@ package co.gobd.loki.loki;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 
     private static final int GPS_ERRORDIALOG_REQUEST = 9001;
+
+    GoogleMap mMap;
+    MapView mMapView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (servicesOk())
-        {
+        if (servicesOk()) {
             Toast.makeText(this, "Ready to map!", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_mapview);
+            mMapView = (MapView) findViewById(R.id.map);
+            mMapView.onCreate(savedInstanceState);
+        } else {
+            setContentView(R.layout.activity_main);
         }
-
-        setContentView(R.layout.activity_main);
-            }
+    }
 
 
     @Override
@@ -52,28 +60,49 @@ public class MainActivity extends ActionBarActivity {
 
 
     // Checks if the Google Play Service is available on the device
-    public boolean servicesOk()
-    {
+    public boolean servicesOk() {
         int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
 
-        if (isAvailable == ConnectionResult.SUCCESS)
-        {
+        if (isAvailable == ConnectionResult.SUCCESS) {
             return true;
-        }
-
-        else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable))
-        {
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
             // Dialog will be selected automatically
             Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, GPS_ERRORDIALOG_REQUEST);
             dialog.show();
-        }
-        else
-        {
+        } else {
             Toast.makeText(this, "Can't connect to Google Play Services", Toast.LENGTH_SHORT).show();
         }
 
         return false;
     }
 
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
 }
