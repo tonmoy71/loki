9ab02ddf62457d7c096ea5cefba19161c9f54299
Fahim Masud Choudhury <tonmoy71@gmail.com>
2015-01-02 11:10:00 +0600

    Google play service checking is added, following Lynda tutorial.

--- app/src/main/java/co/gobd/loki/loki/MainActivity.java
+++ app/src/main/java/co/gobd/loki/loki/MainActivity.java
@@ -1,28 +1,31 @@
 package co.gobd.loki.loki;
 
+import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends ActionBarActivity {
+
+    private static final int GPS_ERRORDIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (servicesOk())
+        {
+            Toast.makeText(this, "Ready to map!", Toast.LENGTH_SHORT).show();
+        }
+
         setContentView(R.layout.activity_main);
-
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-    }
+            }
 
 
     @Override
@@ -48,9 +51,29 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        // Set the marker 
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0)).title("Marker"));
+    // Checks if the Google Play Service is available on the device
+    public boolean servicesOk()
+    {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (isAvailable == ConnectionResult.SUCCESS)
+        {
+            return true;
+        }
+
+        else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable))
+        {
+            // Dialog will be selected automatically
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailable, this, GPS_ERRORDIALOG_REQUEST);
+            dialog.show();
+        }
+        else
+        {
+            Toast.makeText(this, "Can't connect to Google Play Services", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
     }
+
+
 }
